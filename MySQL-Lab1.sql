57fe88df2710ba0f3094b28d586b67d508aebb92
Anita Gashi <anitamulliqii@hotmail.com>
2023-05-22 15:45:34 +0200

    done

---
+++ MySQL-Lab1.sql
@@ -0,0 +1,17 @@
+use sakila;
+
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+
+SELECT title FROM film;
+
+SELECT DISTINCT language_id AS language FROM film;
+SELECT COUNT(*) AS store_count FROM store;
+SELECT COUNT(*) AS employee_count FROM staff;
+SELECT first_name FROM staff;
+
+
+
+
+
